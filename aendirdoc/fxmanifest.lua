43b02b5dfd951fda4d07a5cd50183bf890ea13c4
aendirr <118621878+Aendirr@users.noreply.github.com>
2023-06-09 18:03:02 +0300

    Add files via upload

---
+++ aendirdoc/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'aendirdoc AI Doc [QB]'
+
+version '0.1.0'
+
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+} 
+
+client_script 'fyac.lua'
